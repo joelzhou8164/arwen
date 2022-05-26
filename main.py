625eea297a7a1ebe89013b3c335edcfb22f65cc0
joelz <joelzhoiu@gmail.com>
2022-05-25 20:30:36 -0500

    update

---
+++ main.py
@@ -0,0 +1,12 @@
+
+
+meals = ["morning", "noon", "evening"]
+for x in meals:
+    if x == "morning":
+        y = "food"
+    elif x == "noon":
+        y = "food"
+    elif x == "evening":
+        y = "ribeye"
+
+    print ("Give me", y, "in the", x, "!")
